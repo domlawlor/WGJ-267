a85541d7d8690496f4e59e81c947d44ef8bc9e1b
Tim Day <timday888@gmail.com>
2022-08-19 23:38:19 +1000

    initial project structure

---
+++ Main.gd
@@ -0,0 +1,22 @@
+extends Control
+
+onready var menu : Control = $Menu
+onready var main_2d : Node2D = $Main2D
+
+var level_instance : Node2D
+
+func unload_level():
+	if (is_instance_valid(level_instance)):
+		level_instance.queue_free()
+	level_instance = null
+
+func load_level(level_name : String):
+	unload_level()
+	var level_path := "res://Levels/%s.tscn" % level_name
+	var level_resource := load(level_path)
+	if (level_resource):
+		level_instance = level_resource.instance()
+		main_2d.add_child(level_instance)
+
+func _on_LoadLevel1_pressed():
+	load_level("Level1")
